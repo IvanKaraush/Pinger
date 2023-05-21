a3974a673dddc65b09da9bb28886a3d7a3b869a2
Ivan_Karaush <Karausivan41@gmail.com>
2023-05-21 11:31:08 +0300

    Added logger

---
+++ Logger/LogToFile.cs
@@ -0,0 +1,20 @@
+﻿using Pinger.Interfaces;
+using System;
+using System.IO;
+
+namespace Pinger.Logger
+{
+	public class LogToFile : ILogger
+	{
+		public void Log(string message)
+		{
+			using (var fileStream = new FileStream("log.txt", FileMode.Append))
+			{
+				using (var streamWriter = new StreamWriter(fileStream))
+				{
+					streamWriter.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss")} {message}");
+				}
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ninject;
+using Pinger.Interfaces;
+using Pinger.Logger;
+using System;
 
 namespace Pinger
 {
 	class Program
 	{
+		private static IKernel kernel;
 		static void Main(string[] args)
 		{
+			ConfigureService();
+
+			var logger = kernel.Get<ILogger>();
+
+			logger.Log("Test");
+		}
+
+		private static void ConfigureService()
+		{
+			kernel = new StandardKernel();
+
+			kernel.Bind<ILogger>().To<LogToFile>();
 
 		}
 	}
